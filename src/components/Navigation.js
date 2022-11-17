db408f72397efb22bfaf03b83a47c64cfe96605d
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-11-17 14:55:24 +0100

    Create Soora home page with create react app

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import logo from '../assets/images/logo.png';
+
+const Navigation = () => (
+  <nav>
+    <div>
+      <img src={logo} alt="soora-logo" />
+    </div>
+    <div>
+
+    </div>
+  </nav>
+);
+
+export default Navigation;
